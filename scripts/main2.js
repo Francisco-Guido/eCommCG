9bf6887d3235380364508cfbbaa896d2d33ad27c
franciscoguido-99 <francisconguido@gmail.com>
2021-05-31 00:09:49 -0300

    new Index

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,4 +1,3 @@
-// sidebar toggle
 const btnToggle = document.querySelector('.toggle-btn');
 const btnToggle2 = document.querySelector('.toggle-btn2');
 const btnClose = document.getElementById('close');
@@ -7,17 +6,25 @@ const btnRemove = document.getElementById('remover');
 
 btnToggle.addEventListener('click', function () {
   document.getElementById('sidebar').classList.toggle('active');
-  document.getElementsByClassName('container')[0].classList.toggle('blured');
-  document.getElementsByClassName('navbar')[0].classList.toggle('blured');   
+  document.getElementsByClassName('divBlured')[0].classList.toggle('blured');   
 });
 
 btnClose.addEventListener('click', function () {
     document.getElementById('sidebar').classList.remove('active');
-    document.getElementsByClassName('container')[0].classList.remove('blured');
-    document.getElementsByClassName('navbar')[0].classList.remove('blured');   
+    document.getElementsByClassName('divBlured')[0].classList.remove('blured');  
 });
 
 btnRemove.addEventListener('click', function () {
     document.getElementById('removerCompra').remove()
 });
 
+
+btnToggle2.addEventListener('click', function () {
+  document.getElementById('sidebar2').classList.toggle('active2');
+  document.getElementsByClassName('divBlured')[0].classList.toggle('blured');   
+});
+
+btnClose2.addEventListener('click', function () {
+  document.getElementById('sidebar2').classList.remove('active2');
+  document.getElementsByClassName('divBlured')[0].classList.remove('blured');  
+});
